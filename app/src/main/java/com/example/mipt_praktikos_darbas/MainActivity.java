c060a5925c7609a49e503dcdfbbff20dd0abb34e
Kristupas <kristupas.grigas1@gmail.com>
2023-10-08 15:47:41 +0300

    Antras commit

--- app/src/main/java/com/example/mipt_praktikos_darbas/MainActivity.java
+++ app/src/main/java/com/example/mipt_praktikos_darbas/MainActivity.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonSpalva, buttonTekstas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTekstas = findViewById(R.id.button);
+        buttonTekstas.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv = (TextView) findViewById(R.id.textView);
+                tv.setText("Sveiki visi, cia as");
             }
+        });
+    }
 }
